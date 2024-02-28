568afcc585127c03a51124df9d6ec327000ded17
coderstop244 <152056461+coderstop244@users.noreply.github.com>
2024-02-28 19:07:08 +0530

    Add files via upload

---
+++ project1.js
@@ -0,0 +1,9 @@
+
+const profile={
+    profilName:"SharadhaKhapra",
+    follwers:true,
+    post:195,
+    followers:595000,
+    description:"Enteprenure,Ex-Microsift,DRDO"
+};
+console.log(profile)
